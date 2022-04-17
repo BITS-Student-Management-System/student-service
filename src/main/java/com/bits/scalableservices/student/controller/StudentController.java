461f067b2bc00e625b549b8f3ebba9ea1d1de3d0
Roopika Srinivas <13376391+roopikasrinivas@users.noreply.github.com>
2022-04-17 15:44:07 +0530

    Default data

--- src/main/java/com/bits/scalableservices/student/controller/StudentController.java
+++ src/main/java/com/bits/scalableservices/student/controller/StudentController.java
@@ -1,18 +1,12 @@
 package com.bits.scalableservices.student.controller;
 
+import com.bits.scalableservices.student.VO.StudentRequest;
+import com.bits.scalableservices.student.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.bits.scalableservices.student.VO.StudentRequest;
-import com.bits.scalableservices.student.service.StudentService;
-
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/students")
@@ -32,19 +26,13 @@ public class StudentController {
 		}
 	}
 
-//    @GetMapping("/{id}")
-//    public Student findStudentById(@PathVariable("id") Long studentId){
-//        log.info("Inside findStudentById of StudentController");
-//        return studentService.findStudentById(studentId);
-//    }
-
-	public ResponseEntity<Object> getStudentWithDepartment(@PathVariable("id") Long studentId) {
+	@GetMapping("{id}")
+	public ResponseEntity<Object> getStudentDetails(@PathVariable("id") Long studentId) {
 		log.info("Inside getStudentWithDepartment of StudentController");
 		try {
 			return new ResponseEntity<>(studentService.getStudentWithDepartment(studentId), HttpStatus.OK);
 		} catch (Exception ex) {
 			return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
 		}
-
 	}
 }
